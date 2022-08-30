b0541e175b8f5c3bf80b80b3c3413683768e1843
TinyHustlr <91208042+TinyHustlr@users.noreply.github.com>
2022-08-30 22:44:07 +1000

    Updated HTML and CSS

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// Word array
-const characters = ["reduce", "reference", "freight", "dentist", "punish", "workshop", "offer", "film", "start", "loan", "year", "wisecrack", "stomach", "minute", "disappoint", "omission", "outer", "hold", "flower", "late", "monopoly", "practical", "race", "lead", "bait", "assignment", "provision", "comprehensive", "pause", "performance", "assault", "damn", "monarch", "seal", "abridge", "embox", "temperature", "constellation", "button"];
+// Character array
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
 
 // Get h1 element
 let pass = document.querySelector("h1")
@@ -11,7 +13,7 @@ function getPass() {
 // loops through randomIndex 12 times to pull 12 words from array
         for (i = 0; i < 12; i++) {
         let randomIndex = Math.floor( Math.random() * characters.length )
-        pass.innerHTML += " " + characters[randomIndex]  
+        pass.innerHTML += characters[randomIndex]  
         }   
     }
     
---
+++ phrase.js
@@ -0,0 +1,24 @@
+// Word array
+const words = ["reduce", "reference", "freight", "dentist", "punish", "workshop", "offer", "film", "start", "loan", "year", "wisecrack", "stomach", "minute", "disappoint", "omission", "outer", "hold", "flower", "late", "monopoly", "practical", "race", "lead", "bait", "assignment", "provision", "comprehensive", "pause", "performance", "assault", "damn", "monarch", "seal", "abridge", "embox", "temperature", "constellation", "button"];
+
+// Get h1 element
+let pass = document.querySelector("h1")
+
+// Create two 12 word pass phrases
+function getPass() {
+    
+        
+// loops through randomIndex 12 times to pull 12 words from array
+        for (i = 0; i < 12; i++) {
+        let randomIndex = Math.floor( Math.random() * words.length )
+        pass.innerHTML += " " + words[randomIndex]  
+        }   
+    }
+    
+// executes and loops through getPass function twice adding a 2 new lines each time   
+for (p = 0; p < 2; p++) { 
+getPass()
+pass.innerHTML += "<br /><br />"
+}
+
+
